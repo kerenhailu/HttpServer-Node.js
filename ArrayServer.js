c824f19b8cb95ea888ccf67e642c4943757399e6
kerenhailu <keren4509@gmail.com>
2022-02-21 15:25:13 +0200

    3 way

---
+++ ArrayServer.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+const httpServer = require("http").Server;
+const util=require('util');
+const readFilePromise=util.promisify(fs.readFile);
+
+class ArrayServer extends httpServer {
+  constructor() {
+    super();
+    this.listen(9090);
+    this.on("request", this.callBackReq);
+  }
+  callBackReq(req, res) {
+    readFilePromise("./ArrayNames.json")
+    .then((data)=>{res.end(data)})
+    .catch((error)=>{res.end("errorrrr")})
+  }
+}
+module.exports = new ArrayServer();
+
+
